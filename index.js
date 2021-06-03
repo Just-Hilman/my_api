c939d1807d7a846178c506e7b3272a2b2b4822f2
Just-Hilman <hilman620@yahoo.com>
2021-06-03 10:26:14 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const port = 5000;
+
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/', (req, res) => res.send('Hello'));
+
+app.listen(port, () => console.log(`Server Running on port: http:localhost:${port}`));
